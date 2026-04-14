d667089b69a5c090c4eabb61640ced116056aa17
Damien L <damien.laneelle33@gmail.com>
2026-04-14 09:28:37 +0200

    update

--- src/main/kotlin/com/example/gestion_de_livre/domain/usecase/BookUseCase.kt
+++ src/main/kotlin/com/example/gestion_de_livre/domain/usecase/BookUseCase.kt
@@ -6,8 +6,8 @@ import com.example.gestion_de_livre.domain.port.BookPort
 class BookUseCase(private val bookPort: BookPort) {
 
     fun addBook(title: String, author: String): Book {
-        require(title.isNotBlank()) { "Le titre ne peut pas être vide" }
-        require(author.isNotBlank()) { "L'auteur ne peut pas être vide" }
+        require(title.isNotBlank()) { "Le titre ne peut pas être vide." }
+        require(author.isNotBlank()) { "L'auteur ne peut pas être vide." }
         val book = Book(title = title, author = author)
         bookPort.save(book)
         return book
